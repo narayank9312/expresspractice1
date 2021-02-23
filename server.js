c35e629a26005010877456fb6bcb8aa1e79eb4f8
Narayan Kumar Jha <narayank9312@gmail.com>
2021-02-23 18:04:07 +0530

    formaction
    Signed-off-by: Narayan Kumar Jha <narayank9312@gmail.com>

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('Hello world');
+  let name = 'guest';
+  if (req.query.user) {
+    name = req.query.user;
+  }
+  res.send('Hello world ' + name);
 });
 
 console.log(__dirname + '/public');
